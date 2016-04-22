c701336d63ff15ab1c7ed87d9c1ee98968b2935c
leo <leoelmasmejor@gmail.com>
2016-04-22 11:15:17 -0700

    Creating ManyToMany Relation Betwen Category And Article

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160422175308) do
+ActiveRecord::Schema.define(version: 20160422181147) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -23,6 +23,11 @@ ActiveRecord::Schema.define(version: 20160422175308) do
     t.string   "img_1"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "atricles_categories", id: false, force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
   end
 
   create_table "categories", force: :cascade do |t|
